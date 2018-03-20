e257655efd6f9595003d5dac8d9242ef19132492
lcy362 <lcy362@163.com>
2018-03-20 15:16:19 +0800

    log4j

--- src/main/java/com/solo/subway/data/SubwayInfoParser.java
+++ src/main/java/com/solo/subway/data/SubwayInfoParser.java
@@ -9,13 +9,19 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SubwayInfoParser {
     private static String url = "http://map.amap.com/service/subway?_1469083453978&srhdata=1100_drw_beijing.json";
     private static SubwayInfoParser instance = new SubwayInfoParser();
+    private static Logger logger = LoggerFactory.getLogger(SubwayInfoParser.class);
+
+    private Map<String, String> lineName = new HashMap<String, String>();
     private SubwayInfoParser(){}
 
     public static SubwayInfoParser getInstace() {
@@ -24,10 +30,10 @@ public class SubwayInfoParser {
 
     public void parse() throws IOException {
         String result = HttpUtil.httpGet(url);
-        System.out.println(result);
+        logger.info(result);
         Map<String, Object> json = (Map<String, Object>) JSON.parse(result);
 
-        System.out.println(json);
+        System.out.println(json.get("l"));
     }
 
     public static void main(String args[]) throws IOException {
