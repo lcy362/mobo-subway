6580c41270c6c2778071a5197e6dfa2385e2560e
lcy362 <lcy362@163.com>
2024-03-05 19:27:28 +0800

    calculate meeting point

--- src/main/java/com/mobo/mobosubway/view/MeetingPointView.java
+++ src/main/java/com/mobo/mobosubway/view/MeetingPointView.java
@@ -1,16 +1,20 @@
 package com.mobo.mobosubway.view;
 
 import com.mobo.mobosubway.service.PathService;
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Route(value = "meetingPoint", layout = MainView.class)
 @PageTitle("汇合点规划")
@@ -47,7 +51,19 @@ public class MeetingPointView  extends VerticalLayout {
             input.add(layout);
         });
 
-        add(input, addItem);
+        HorizontalLayout result = new HorizontalLayout();
+        Text meetingPoints = new Text("");
+        result.add(new H3("结果为: "), meetingPoints);
+
+        Button cal = new Button("计算");
+        cal.addClickListener(click -> {
+            List<String> statoinList = stations.stream().map(TextField::getValue).collect(Collectors.toList());
+            List<Double> distanceList = distances.stream().map(TextField::getValue).map(Double::parseDouble).collect(Collectors.toList());
+            List<String> calresult = pathService.getAvailableMeetingPoint(statoinList, distanceList);
+            meetingPoints.setText(StringUtils.join(calresult, ","));
+        });
+
+        add(input, addItem, cal, result);
     }
 
 
